5f9660d0fc1647e28ce9dccc9a243daa795b5a21
Aleksey-Kolpakov <gmsaleksei@gmail.com>
2021-01-19 18:44:27 +0200

    hw13

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,6 +1,12 @@
-const key = '19945506-a6680bfa60c04d980657bbe54';
-const apiUrl = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
-const searchQuerry = 'dog';
-const pageNumber = 1;
-fetch(`${apiUrl}&q=${searchQuerry}&page=${pageNumber}&per_page=12&key=${key}`)
-    .then(response => response.json()).then(data => { console.log(data); });
+const apiService = {
+    key: '19945506-a6680bfa60c04d980657bbe54',
+    apiUrl: 'https://pixabay.com/api/?image_type=photo&orientation=horizontal',
+    searchQuerry: 'dog',
+    pageNumber: 1,
+    fetchApi() {
+        return fetch(`${this.apiUrl}&q=${this.searchQuerry}&page=${this.pageNumber}&per_page=12&key=${this.key}`)
+            .then(response => response.json())
+            .then(({ hits }) => hits);
+    }
+};
+apiService.fetchApi().then(data => { console.log(data); });
