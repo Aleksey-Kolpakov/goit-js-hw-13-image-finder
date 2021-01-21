08be6e4d5df0a454e33d41b928504ff5a7fa8659
Aleksey-Kolpakov <gmsaleksei@gmail.com>
2021-01-21 18:19:27 +0200

    ed

--- src/js/galeryLightBox.js
+++ src/js/galeryLightBox.js
@@ -2,10 +2,12 @@ import * as basicLightbox from 'basiclightbox';
 import "../../node_modules/basiclightbox/dist/basicLightbox.min.css"
 
 function galeryLightBox(event) {
-    const image = event.target.dataset.largeimage;
-    const instan = basicLightbox.create(`
+    if (event.target.nodeName === "IMG") {
+        const image = event.target.dataset.largeimage;
+        const instan = basicLightbox.create(`
     <img src="${image}" width="100%" height="100%">
 `);
-    instan.show();
+        instan.show();
+    }
 };
 export default galeryLightBox;
